9c25af7af4bda462b8328c7a0a7c7fc9801c6206
skytthe <skytthe@gmail.com>
2025-04-03 15:25:40 +0200

    2025 round 16

--- 2025/round16/leviathan_mindscape.py
+++ 2025/round16/leviathan_mindscape.py
@@ -49,22 +49,35 @@ class Face:
     def getAbsorption(self):
         return self.absorption
 
+    def getDominantSum(self):
+        return max(max(sum(line) for line in self.grid),
+                   max(sum(line) for line in zip(*self.grid)))
+
+    def update_cell(self, cell, value):
+        return (cell + value - 1) % 100 + 1
+
     def update(self, instr):
         if instr[0] == "FACE":
             value = int(instr[2])
-            self.grid[:] = [[cell + value for cell in row]
-                            for row in self.grid]
+            self.grid = [
+                [self.update_cell(cell, value) for cell in row]
+                for row in self.grid
+            ]
             self.absorption += value * self.size**2
+
         elif instr[0] == "ROW":
-            offset = int(instr[1])-1
+            offset = int(instr[1]) - 1
             value = int(instr[3])
-            self.grid[offset] = [cell + value for cell in self.grid[offset]]
+            self.grid[offset] = [
+                self.update_cell(cell, value) for cell in self.grid[offset]
+            ]
             self.absorption += value * self.size
+
         elif instr[0] == "COL":
-            offset = int(instr[1])-1
+            offset = int(instr[1]) - 1
             value = int(instr[3])
             for row in self.grid:
-                row[offset] += value
+                row[offset] = self.update_cell(row[offset], value)
             self.absorption += value * self.size
 
     def rotateCW(self):
@@ -72,6 +85,9 @@ class Face:
 
     def rotateCCW(self):
         self.grid = [list(row) for row in zip(*self.grid)][::-1]
+
+    def rotate180(self):
+        self.grid = [row[::-1] for row in self.grid[::-1]]
 
     def __str__(self):
         return "\n".join(" ".join(str(cell) for cell in row) for row in self.grid)
@@ -94,22 +110,46 @@ class Cube:
     def getCurrentFaceId(self):
         return self.current.getId()
 
-    def update(self, instr):
-        self.current.update(instr)
+    def update(self, instr, full_cube=False):
+        if instr[0] == "FACE":
+            self.current.update(instr)
+
+        elif instr[0] == "ROW":
+            self.current.update(instr)
+            if full_cube:
+                self.right.update(instr)
+                self.back.rotate180()
+                self.back.update(instr)
+                self.back.rotate180()
+                self.left.update(instr)
+
+        elif instr[0] == "COL":
+            self.current.update(instr)
+            if full_cube:
+                self.top.update(instr)
+                self.back.update(instr)
+                self.bottom.update(instr)
 
     def twist(self, t):
         if t == "L":
             self.current, self.left, self.back, self.right = self.left, self.back, self.right, self.current
-            self.top.rotateCW()
-            self.bottom.rotateCCW()
-        elif t == "R":
-            self.current, self.right, self.back, self.left = self.right, self.back, self.left, self.current
             self.top.rotateCCW()
             self.bottom.rotateCW()
+            self.back.rotate180()
+            self.left.rotate180()
+
+        elif t == "R":
+            self.current, self.right, self.back, self.left = self.right, self.back, self.left, self.current
+            self.top.rotateCW()
+            self.bottom.rotateCCW()
+            self.back.rotate180()
+            self.right.rotate180()
+
         elif t == "D":
             self.current, self.bottom, self.back, self.top = self.bottom, self.back, self.top, self.current
-            self.left.rotateCW()
-            self.right.rotateCCW()
+            self.left.rotateCCW()
+            self.right.rotateCW()
+
         elif t == "U":
             self.current, self.top, self.back, self.bottom = self.top, self.back, self.bottom, self.current
             self.left.rotateCW()
@@ -123,15 +163,28 @@ class Cube:
     def getAbsorptions(self):
         return [f.getAbsorption() for f in self.faces]
 
+    def getDominantSums(self):
+        return [f.getDominantSum() for f in self.faces]
+
 
 cube = Cube(80)
 
 for instr, twist in zip_longest(instructions, twists, fillvalue=None):
-    # print(f"{instr} then {twist}")
     cube.update(instr)
     if twist:
         cube.twist(twist)
-        pass
 
-
+# part 1
 print(math.prod(sorted(cube.getAbsorptions(), reverse=True)[:2]))
+# part 2
+print(math.prod(sorted(cube.getDominantSums(), reverse=True)))
+
+cube = Cube(80)
+
+for instr, twist in zip_longest(instructions, twists, fillvalue=None):
+    cube.update(instr, True)
+    if twist:
+        cube.twist(twist)
+
+# part 3
+print(math.prod(sorted(cube.getDominantSums(), reverse=True)))
