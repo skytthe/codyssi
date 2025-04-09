b9a2b57d4027ce708a69550945ecc11d44f256bd
skytthe <skytthe@gmail.com>
2025-04-09 10:33:26 +0200

    2025 round 17, part2 code structure

--- 2025/round17/spiralling_stairs.py
+++ 2025/round17/spiralling_stairs.py
@@ -1,5 +1,6 @@
 import re
 from functools import cache
+from collections import defaultdict
 
 input = """S1 : 0 -> 6 : FROM START TO END
 S2 : 2 -> 3 : FROM S1 TO S1
@@ -13,18 +14,29 @@ with open("2025/round17/spiralling_stairs.txt") as f:
 stairs = [tuple(map(int, re.findall(r"\d+", line)))
           for line in input[0].splitlines()]
 
+
+def makeStair(name, frm, to, start, end):
+    pass
+
+
+graph = defaultdict(list)
 for stair in stairs:
     if stair[0] == 1:
         start_level = stair[1]
         goal_level = stair[2]
     else:
-        pass
+        id, from_level, to_level, start_stair, end_stair = stair
 
 possible_moves = tuple(map(int, re.findall(r"\d+", input[1])))
 
 
 @cache
-def move(pos, goal, possibleMoves):
+def takeStep(pos, length, possibleMoves):
+    pass
+
+
+@cache
+def climbStairs(pos, goal, possibleMoves):
     if pos == goal:
         return 1
     elif pos > goal:
@@ -32,14 +44,8 @@ def move(pos, goal, possibleMoves):
     else:
         moves = 0
         for m in possibleMoves:
-            moves += move(pos+m, goal, possibleMoves)
+            moves += climbStairs(pos+m, goal, possibleMoves)
         return moves
 
 
-print(move(start_level, goal_level, possible_moves))
-
-
-# print(move(0, 6, (1, 3)))
-# print(move(0, 6, (1, 2)))
-# print(move(0, 99, (1, 3, 5, 6)))
-# print(move(0, 98, (1, 2, 3, 5, 7)))
+print(climbStairs(start_level, goal_level, possible_moves))
